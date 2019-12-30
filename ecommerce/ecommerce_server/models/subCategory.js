c76998cd52430e08285dceaf61d0b5136b0de34d
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-12-30 10:54:20 +0700

    create model: subCategory

---
+++ ecommerce/ecommerce_server/models/subCategory.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const subCategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('SubCategory', subCategorySchema);
