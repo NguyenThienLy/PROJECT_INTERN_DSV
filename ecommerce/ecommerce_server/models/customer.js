a0ddc23dca71d2130b8e0b740ce199a4d9a94520
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-12-30 10:16:29 +0700

    created modals: product, customer

---
+++ ecommerce/ecommerce_server/models/customer.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String, 
+        ref: ['active', 'deactive'], 
+        default: 'active'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
---
+++ ecommerce/ecommerce_server/models/product.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Brand" }],
+        default: []
+    },
+    color: {
+        type: String,
+        enum: ['#ff5f6d', 'rgba(255, 195, 113, 0.5)', 'rgba(95, 109, 255, 0.5)', 'rgba(255, 161, 95, 0.5)', 'rgba(61, 61, 63, 0.5)'],
+        default: []
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L'],
+        default: []
+    },
+    subCategory: {
+        type: Schema.Types.ObjectId, 
+        ref: "SubCategory", 
+        required: true
+    },
+    subImage: {
+        type: [{ type: string}],
+        default: []
+    },
+    mainImage: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        default: 1,
+        required: true
+    },
+    quantity: {
+        type: Number, 
+        default: 1,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['In-store', 'Out of stock'],
+        default: 'In-store'
+    },
+    description: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
