f4d426669103cdde272af45834b0cbd0738470f9
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2020-01-04 12:55:17 +0700

    add Input, Select, Text Area

--- ecommerce/ecommerce_client/src/pages/seller/product/add/add.component.js
+++ ecommerce/ecommerce_client/src/pages/seller/product/add/add.component.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Avatar, Layout, Col, Row, Button, Upload, Icon, Modal } from 'antd';
+import {
+    Input,
+    Layout,
+    Col,
+    Row,
+    Button,
+    Upload,
+    Icon,
+    Modal,
+    Select
+} from 'antd';
 import {
     SideNav,
     HeaderSeller,
@@ -23,6 +33,8 @@ export function ProductAdd({
 
 }) {
     const { Content } = Layout;
+    const { TextArea } = Input;
+    const { Option } = Select;
 
     const [previewImage, setPreviewImage] = useState('');
     const [previewVisible, setPreviewVisible] = useState(false);
@@ -55,37 +67,153 @@ export function ProductAdd({
             </Modal>
 
             <Content className="body-page">
-                <Col span={4}>
-                    <SideNav selectDefault="4" />
-                </Col>
+                <Row>
+                    <Col span={4} className="container-side-nav">
+                        <SideNav selectDefault="4" />
+                    </Col>
 
-                <Col span={20} className="container-content">
-                    <HeaderSeller namePage="Products/Add" />
+                    <Col span={20} className="container-content">
+                        <HeaderSeller namePage="Products/Add" />
 
-                    <Row className="container-props">
-                        <Row className="container-photos">
-                            <Col className="name-props" span={2}>PHOTOS</Col>
-                            <Col span={21} offset={1}>
-                                <Upload
-                                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                                    listType="picture-card"
-                                    fileList={fileList}
-                                    onPreview={handlePreview}
-                                    onChange={handleChange}
-                                >
-                                    {fileList.length >= 4 ? null : uploadButton}
-                                </Upload>
-                            </Col>
+                        <Row className="container-props">
+                            <Row className="container-photos">
+                                <Col className="name-props photos" span={3}><p>PHOTOS</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Upload
+                                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                        listType="picture-card"
+                                        fileList={fileList}
+                                        onPreview={handlePreview}
+                                        onChange={handleChange}
+                                    >
+                                        {fileList.length >= 4 ? null : uploadButton}
+                                    </Upload>
+
+                                    <p className="note">You can add up to 4 photos. The 1st photo will be set as cover (main photo).</p>
+                                </Col>
+                            </Row>
+
+                            <Row className="container-input">
+                                <Col className="name-props" span={3}><p>NAME</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Input className="input" placeholder="Enter name product..." />
+                                </Col>
+                            </Row>
+
+                            <Row className="container-select">
+                                <Col className="name-props" span={3}><p>CATEGORIES</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Select
+                                        className="select"
+                                        mode="multiple"
+                                        placeholder="Please select categories"
+                                        defaultValue={["Men", "Girls"]}
+                                    >
+                                        <Option key="Men">Men</Option>
+                                        <Option key="Girls">Girls</Option>
+                                        <Option key="Boys">Boys</Option>
+                                        <Option key="Ladies">Ladies</Option>
+                                    </Select>
+                                </Col>
+                            </Row>
+
+                            <Row className="container-select">
+                                <Col className="name-props" span={3}><p>SUB CATEGORIES</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Select
+                                        className="select"
+                                        mode="multiple"
+                                        placeholder="Please select sub categories"
+                                        defaultValue={["Men", "Girls"]}
+                                    >
+                                        <Option key="Men">Men</Option>
+                                        <Option key="Girls">Girls</Option>
+                                        <Option key="Boys">Boys</Option>
+                                        <Option key="Ladies">Ladies</Option>
+                                    </Select>
+                                </Col>
+                            </Row>
+
+                            <Row className="container-select">
+                                <Col className="name-props" span={3}><p>BRAND</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Select
+                                        className="select"
+                                        defaultValue="Zara" >
+                                        <Option value="H&M">H&M</Option>
+                                        <Option value="Dior">Dior</Option>
+                                        <Option value="VL">VL</Option>
+                                    </Select>
+                                </Col>
+                            </Row>
+
+                            <Row className="container-input">
+                                <Col className="name-props" span={3}><p>PRICE($)</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Input className="input" placeholder="Enter price product..." />
+                                </Col>
+                            </Row>
+
+                            <Row className="container-select">
+                                <Col className="name-props" span={3}><p>SIZE</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Select
+                                        className="select"
+                                        mode="multiple"
+                                        placeholder="Please select size"
+                                        defaultValue={["S", "M", "L"]}
+                                    >
+                                        <Option key="S">S</Option>
+                                        <Option key="M">M</Option>
+                                        <Option key="L">L</Option>
+                                    </Select>
+                                </Col>
+                            </Row>
+
+                            <Row className="container-select">
+                                <Col className="name-props" span={3}><p>COLORS</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Select
+                                        className="select"
+                                        mode="multiple"
+                                        placeholder="Please select colors"
+                                        defaultValue={["Pink"]}
+                                    >
+                                        <Option key="Pink">Pink</Option>
+                                        <Option key="Pale yellow">Pale yellow</Option>
+                                        <Option key="Pale blue">Pale blu</Option>
+                                        <Option key="Orange">Orange</Option>
+                                        <Option key="Pale black">Pale black</Option>
+                                        {/* {['Pink', 'Pale yellow', 'Pale blue', 'Orange', 'Pale black']} */}
+                                    </Select>
+                                </Col>
+                            </Row>
+
+                            <Row className="container-input">
+                                <Col className="name-props" span={3}><p>QUANTITY</p></Col>
+                                <Col span={20} offset={1}>
+                                    <Input className="input" placeholder="Enter quantity product..." />
+                                </Col>
+                            </Row>
+
+                            <Row className="container-text-area">
+                                <Col className="name-props" span={3}><p>DESCRIPTION</p></Col>
+                                <Col span={20} offset={1}>
+                                    <TextArea className="text-area" rows={4} placeholder="Enter description product..." />
+                                </Col>
+                            </Row>
+
+                            <Row className="container-btn">
+                                <Col span={4} offset={15}>
+                                    <Button className="btn-secondary" type="primary">Cancel</Button>
+                                </Col>
+                                <Col span={4} offset={1}>
+                                    <Button className="btn-primary" type="primary">Complete</Button>
+                                </Col>
+                            </Row>
                         </Row>
-
-                        <Row>
-                            <Col  className="name-props" span={2}>NAME</Col>
-                            <Col span={21} offset={1}>
-                            </Col>
-                        </Row>
-                    </Row>
-
-                </Col>
+                    </Col>
+                </Row>
             </Content>
         </div >
     );
--- ecommerce/ecommerce_client/src/pages/seller/product/main/main.component.js
+++ ecommerce/ecommerce_client/src/pages/seller/product/main/main.component.js
@@ -20,23 +20,25 @@ export function ProductMain({
     return (
         <div className="product-main-page">
             <Content className="body-page">
-                <Col span={4}>
-                    <SideNav selectDefault="3"/>
-                </Col>
+                <Row>
+                    <Col span={4} className="container-side-nav">
+                        <SideNav selectDefault="3" />
+                    </Col>
 
-                <Col span={20} className="container-content">
-                    <HeaderSeller namePage="Products"/>
-                    <Row className="container-header">
-                        <Col span={2} className="container-titler">SORT BY</Col>
-                        <Col span={3} className="container-btn"><Order/></Col>
-                        <Col span={5} offset={6} className="container-search"><SearchSeller/></Col>
-                        <Col span={4} className="container-btn"><Button style={{backgroundColor: '#ffa15f', color: 'white'}} className="btn-primary" type="primary" icon="plus">Add product</Button></Col>
-                        <Col span={3} className="container-btn"><Button style={{color: '#ffa15f'}} className="btn-primary" type="primary" icon="export">Export</Button></Col>
-                    </Row>
-                    <Row className="container-table">
-                        <ProductTable/>
-                    </Row>
-                </Col>
+                    <Col span={20} className="container-content">
+                        <HeaderSeller namePage="Products" />
+                        <Row className="container-header">
+                            <Col span={2} className="container-titler">SORT BY</Col>
+                            <Col span={3} className="container-btn"><Order /></Col>
+                            <Col span={5} offset={6} className="container-search"><SearchSeller /></Col>
+                            <Col span={4} className="container-btn"><Button style={{ backgroundColor: '#ffa15f', color: 'white' }} className="btn-primary" type="primary" icon="plus">Add product</Button></Col>
+                            <Col span={3} className="container-btn"><Button style={{ color: '#ffa15f' }} className="btn-primary" type="primary" icon="export">Export</Button></Col>
+                        </Row>
+                        <Row className="container-table">
+                            <ProductTable />
+                        </Row>
+                    </Col>
+                </Row>
             </Content>
         </div >
     );
